1f7dacbcc66c93c234d72ab6776a7fb4739df452
A-DUYVESTYN <a.duyvestyn@gmail.com>
2022-07-28 19:15:34 +0000

    add getAllReservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -42,7 +42,7 @@ const getUserWithId = function(id) {
   return pool
     .query('SELECT * FROM users WHERE id = $1', [id])
     .then((result) => {
-      console.log(result.rows[0]);
+      // console.log(result.rows[0]);
       return result.rows[0]
     })
     .catch((err) => {
@@ -58,7 +58,6 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  console.log(user)
   return pool
   .query('INSERT INTO users (name, email, password) VALUES($1, $2, $3) RETURNING *',[user.name, user.email,user.password])
   .then((result) => {
@@ -79,7 +78,26 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  console.log('guest_id: ', guest_id)
+  return pool
+  .query(`SELECT reservations.*, properties.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;`, [guest_id, limit])
+  .then((result) => {
+    console.log(result.rows);
+    return result.rows
+  })
+  .catch((err) => {
+    console.log("error in getAllReservations")
+    console.log(err.message);
+  });
+
+  // return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
 
@@ -95,7 +113,7 @@ const getAllProperties = (options, limit = 10) => {
   return pool
     .query('SELECT * FROM properties LIMIT $1', [limit])
     .then((result) => {
-      console.log(result.rows);
+      // console.log(result.rows);
       return result.rows
     })
     .catch((err) => {
